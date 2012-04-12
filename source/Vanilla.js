81c9f2da8879165d6324be99c23551c474664146
Ian Macali <ianmacalinao@gmail.com>
2012-04-12 02:38:24 -0500

    Added materials.

---
+++ source/Vanilla.js
@@ -0,0 +1,12 @@
+mm.defineMod("Vanilla", {
+    description: "A Javascript mod that attempts to replicate Vanilla Minecraft.",
+    author: "AlbireoX",
+    revision: 1,
+    url: "http://simplyian.com",
+    dependencies: [
+        "VanillaMaterials"
+    ]
+}, function(mod) {
+
+
+});
---
+++ source/material/VanillaMaterials.js
@@ -0,0 +1,7 @@
+mm.defineMod("VanillaMaterials", {
+    description: "A Javascript mod that contains all materials found in Vanilla Minecraft.",
+    author: "AlbireoX",
+    revision: 1,
+    url: "http://simplyian.com"
+}, function(mod) {
+});
