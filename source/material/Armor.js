5ba914dc517ea52d076edff308b4af194f6abab2
Ian Macali <ianmacalinao@gmail.com>
2012-04-17 12:49:08 -0500

    Armor

---
+++ source/material/Armor.js
@@ -0,0 +1,11 @@
+mm.definePartType("Armor", {
+    description: "Armor item",
+    author: "AlbireoX",
+    revision: 1
+}, {
+    protection: 0
+}, {
+    //No methods
+}, function(part) {
+    //Nothing to initialize yet
+});
