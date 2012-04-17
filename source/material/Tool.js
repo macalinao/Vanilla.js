ece4967b8fd783985dcd76189d5d0764e60efb3a
Ian Macali <ianmacalinao@gmail.com>
2012-04-17 12:35:55 -0500

    More tool stuff.

--- source/material/Tool.js
+++ source/material/Tool.js
@@ -1,7 +1,8 @@
 mm.definePartType("Tool", {
     description: "Represents a usable item that is made of a material.",
     author: "AlbireoX",
-    revision: 1
+    revision: 1,
+    uses: ["Item"]
 }, {
     durability: 0,
     strengthModifiers: []
@@ -16,5 +17,9 @@ mm.definePartType("Tool", {
         part.props.strengthModifiers[block] = modifier;
     }
 }, function(part) {
-
+    part.addItem("tool", {
+        name: part.props.name
+    }, function(item) {
+        //TODO add durability etc.
+    });
 });
